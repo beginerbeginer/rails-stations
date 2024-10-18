95e24676d89205cea08cb727c4b66835b523eae5
beginerbeginer <miyabi6125@gmail.com>
2024-10-19 01:08:10 +0900

    テスト成功：問題03

--- app/controllers/admin/movies_controller.rb
+++ app/controllers/admin/movies_controller.rb
@@ -2,4 +2,28 @@ class Admin::MoviesController < ApplicationController
   def index
     @movies = Movie.all
   end
+
+  def new
+    @movie = Movie.new
+  end
+
+  def create
+    @movie = Movie.new(movie_params)
+
+    if @movie.save
+      redirect_to admin_movies_path, notice: '映画が登録されました'
+    else
+      flash[:alert] = '映画の登録に失敗しました。同じタイトルの映画は登録できません'
+      render :new
+    end
+  rescue StandardError => e
+    flash[:alert] = "エラーが発生しました： #{e.message}"
+    render :new
+  end
+
+  private
+
+  def movie_params
+    params.require(:movie).permit(:name, :year, :description, :image_url, :is_showing)
+  end
 end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,4 +1,11 @@
 class Movie < ApplicationRecord
+  validates :name, presence: true, uniqueness: true
+  validates :year, presence: true
+  validates :description, presence: true
+  validates :image_url, presence: true
+  # is_showingはtrue又はfalseでなければならない
+  validates :is_showing, inclusion: { in: [true, false] }
+
   def showing_status
     is_showing ? '上映中' : '上映予定'
   end
