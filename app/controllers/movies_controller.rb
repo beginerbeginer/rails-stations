e2754aa012b8047008909a4378faba7dff5de1a1
beginerbeginer <miyabi6125@gmail.com>
2024-10-19 20:24:50 +0900

    テスト成功：問題08

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -2,4 +2,9 @@ class MoviesController < ApplicationController
   def index
     @movies = Movie.search(keyword: params[:keyword], is_showing: params[:is_showing])
   end
+
+  def show
+    @movie = Movie.find(params[:id])
+    @schedules = @movie.schedules
+  end
 end
